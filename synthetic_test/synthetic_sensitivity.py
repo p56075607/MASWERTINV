325f3299772a70ebf5e56ebcbdaa70fe8fbad97c
p56075607 <36730477+p56075607@users.noreply.github.com>
2024-03-05 10:37:30 +0800

    clean unnecessary codes

--- synthetic_test/synthetic_sensitivity.py
+++ synthetic_test/synthetic_sensitivity.py
@@ -192,171 +192,28 @@ for n, depth_i in enumerate(layer_depth):
     ax.set_xlim(0,100)
     ax.text(5,-25,'Interface at {:.0f} m'.format(depth_i)
             ,fontweight="bold", size=16)  
-# %%
-# Calculate the resistivity relative difference
-# Subplot:Layered mesh resistivity residual
-fig, axs = plt.subplots(nrows=5, ncols=1, figsize=(16, 12),constrained_layout=True)
+# # %%
+# # Calculate the resistivity relative difference
+# # Subplot:Layered mesh resistivity residual
+# fig, axs = plt.subplots(nrows=5, ncols=1, figsize=(16, 12),constrained_layout=True)
 
-for depth_i, ax in zip(layer_depth, axs.ravel()):
-    interface2 = mt.createLine(start=[left_edge, depth_i], end=[right_edge, depth_i])
-    residual_layer_grid = ((rho_layer_grid - rho_grid)/rho_grid)*100
-    pg.viewer.showMesh(grid,data=residual_layer_grid,ax=ax,
-                        label='Relative resistivity difference (%)',
-                    #     logScale=True, 
-                        cMap='bwr', 
-                        cMin=-35,cMax=35,
-                        xlabel="x (m)", ylabel="z (m)",orientation = 'vertical',
-                        )
-    ax.set_title('Structured constrained resistivity difference profile',fontweight="bold", size=16)
-    ax.add_patch(plt.Polygon(triangle_left,color='white'))
-    ax.add_patch(plt.Polygon(triangle_right,color='white'))
-    pg.show(interface2,ax=ax)
-    ax.plot(np.array(pg.x(data)), np.array(pg.z(data)),'ko')
-    ax.set_ylim(-30, 0)    
-    ax.set_xlim(0,100)
-    ax.text(5,-25,'Interface at {:.0f} m'.format(depth_i)
-            ,fontweight="bold", size=16)
+# for depth_i, ax in zip(layer_depth, axs.ravel()):
+#     interface2 = mt.createLine(start=[left_edge, depth_i], end=[right_edge, depth_i])
+#     residual_layer_grid = ((rho_layer_grid - rho_grid)/rho_grid)*100
+#     pg.viewer.showMesh(grid,data=residual_layer_grid,ax=ax,
+#                         label='Relative resistivity difference (%)',
+#                     #     logScale=True, 
+#                         cMap='bwr', 
+#                         cMin=-35,cMax=35,
+#                         xlabel="x (m)", ylabel="z (m)",orientation = 'vertical',
+#                         )
+#     ax.set_title('Structured constrained resistivity difference profile',fontweight="bold", size=16)
+#     ax.add_patch(plt.Polygon(triangle_left,color='white'))
+#     ax.add_patch(plt.Polygon(triangle_right,color='white'))
+#     pg.show(interface2,ax=ax)
+#     ax.plot(np.array(pg.x(data)), np.array(pg.z(data)),'ko')
+#     ax.set_ylim(-30, 0)    
+#     ax.set_xlim(0,100)
+#     ax.text(5,-25,'Interface at {:.0f} m'.format(depth_i)
+#             ,fontweight="bold", size=16)
 
-
-# %%
-fig, ((ax1, ax2), (ax3, ax4), (ax5, ax6)) = plt.subplots(3,2, figsize=(16,8),constrained_layout=True)
-ax2.axis('off')
-# Subplot 1:Original resistivity model
-pg.viewer.showMesh(mesh, rhomap,ax=ax1,
-                    label='Resistivity ($\Omega m$)',
-                    logScale=True,cMap='jet',cMin=50,cMax=150,
-                    xlabel="x (m)", ylabel="z (m)",orientation = 'vertical')
-ax1.set_title('Original resistivity model profile',fontweight="bold", size=16)
-
-# Subplot 3:normal grid 
-rho_normal_grid = pg.interpolate(mesh2, mgr2.model, grid.cellCenters())
-pg.viewer.showMesh(mesh2, mgr2.model,#grid,data=rho_normal_grid,
-                   ax=ax3,
-                    label='Resistivity ($\Omega m$)',
-                    logScale=True,cMap='jet',cMin=50,cMax=150,
-                    xlabel="x (m)", ylabel="z (m)",orientation = 'vertical')
-ax3.set_title('Normal mesh inverted resistivity profile',fontweight="bold", size=16)
-
-# plot a triangle
-ax3.add_patch(plt.Polygon(triangle_left,color='white'))
-ax3.add_patch(plt.Polygon(triangle_right,color='white'))
-ax3.plot(np.array(pg.x(data)), np.array(pg.z(data)),'ko')
-ax3.set_ylim(-30, 0)
-ax3.plot(pg.x(interface2.nodes()),pg.y(interface2.nodes()),'--k')
-ax3.set_ylim(-30, 0)
-ax3.set_xlim(0,100)
-
-# Subplot 5:structured constrained grid 
-rho_layer_grid = pg.interpolate(mgr3.paraDomain, mgr3.model, grid.cellCenters())
-pg.viewer.showMesh(mgr3.paraDomain, mgr3.model,#grid,data=rho_layer_grid,
-                    ax=ax5,
-                    label='Resistivity ($\Omega m$)',
-                    logScale=True,cMap='jet',cMin=50,cMax=150,
-                    xlabel="x (m)", ylabel="z (m)",orientation = 'vertical')
-ax5.set_title('Structured constrained inverted resistivity profile',fontweight="bold", size=16)
-ax5.add_patch(plt.Polygon(triangle_left,color='white'))
-ax5.add_patch(plt.Polygon(triangle_right,color='white'))
-pg.show(interface2,ax=ax5)
-ax5.plot(np.array(pg.x(data)), np.array(pg.z(data)),'ko')
-ax5.set_ylim(-30, 0)
-ax5.set_xlim(0,100)
-# Calculate the resistivity relative difference
-# Subplot 4:Normal mesh resistivity residual
-residual_normal_grid = ((rho_normal_grid - rho_grid)/rho_grid)*100
-pg.viewer.showMesh(grid,data=residual_normal_grid,ax=ax4,
-                    label='Relative resistivity difference (%)',
-                #     logScale=True, 
-                    cMap='bwr', 
-                    cMin=-35,cMax=35,
-                    xlabel="x (m)", ylabel="z (m)",orientation = 'vertical')
-ax4.set_title('Normal mesh resistivity difference profile',fontweight="bold", size=16)
-ax4.add_patch(plt.Polygon(triangle_left,color='white'))
-ax4.add_patch(plt.Polygon(triangle_right,color='white'))
-ax4.plot(np.array(pg.x(data)), np.array(pg.z(data)),'ko')
-ax4.plot(pg.x(interface2.nodes()),pg.y(interface2.nodes()),'--k')
-ax4.set_ylim(-30, 0)
-ax4.set_xlim(0,100)
-
-# Subplot 6:Layered mesh resistivity residual
-residual_layer_grid = ((rho_layer_grid - rho_grid)/rho_grid)*100
-pg.viewer.showMesh(grid,data=residual_layer_grid,ax=ax6,
-                    label='Relative resistivity difference (%)',
-                #     logScale=True, 
-                    cMap='bwr', 
-                    cMin=-35,cMax=35,
-                    xlabel="x (m)", ylabel="z (m)",orientation = 'vertical',
-                    )
-ax6.set_title('Structured constrained resistivity difference profile',fontweight="bold", size=16)
-ax6.add_patch(plt.Polygon(triangle_left,color='white'))
-ax6.add_patch(plt.Polygon(triangle_right,color='white'))
-pg.show(interface2,ax=ax6)
-ax6.plot(np.array(pg.x(data)), np.array(pg.z(data)),'ko')
-ax6.set_ylim(-30, 0)
-
-
-# fig.savefig(join('results','synthetic_compare.png'), dpi=300, bbox_inches='tight', transparent=True)
-# %%
-# # Plot profile using contour
-# mesh_X, mesh_Y = np.meshgrid(mesh_x,mesh_y)
-# fig, ((ax1, ax2), (ax3, ax4), (ax5, ax6)) = plt.subplots(3,2, figsize=(16,8),constrained_layout=True)
-# ax2.axis('off')
-# # Subplot 1:Original resistivity model
-# pg.viewer.showMesh(mesh, rhomap,ax=ax1,
-#                     label='Resistivity ($\Omega m$)',
-#                     logScale=True,cMap='jet',cMin=50,cMax=150,
-#                     xlabel="x (m)", ylabel="z (m)",orientation = 'vertical')
-# ax1.set_title('Original resistivity model profile')
-
-# rho_contour = np.log10(np.reshape( pg.interpolate(mesh2, mgr2.model, grid.positions()) ,(len(mesh_y),len(mesh_x))))
-# clim = [50, 150]
-# levels = 32
-# contour_plot = ax3.contourf(mesh_X, mesh_Y, rho_contour,
-#             levels = levels,
-#             cmap='jet'
-#             ,cMin=clim[0],cMax=clim[0]
-#             )
-# ax3.set_title('Normal mesh inverted resistivity profile')
-# ax3.set_ylim(-30, 0)
-# divider = make_axes_locatable(ax3)
-# cbaxes = divider.append_axes("right", size="3%", pad=.1)
-# m = plt.cm.ScalarMappable(cmap=plt.cm.jet)
-# m.set_array(rho_contour)
-# m.set_clim(np.log10(clim[0]),np.log10(clim[1]))
-# cb = plt.colorbar(m, boundaries=np.linspace(np.log10(clim[0]),np.log10(clim[1]), levels),cax=cbaxes)
-# cb.ax.set_yticks(np.linspace(np.log10(clim[0]),np.log10(clim[1]),5))
-# cb.ax.set_yticklabels(['{:.0f}'.format(10**x) for x in cb.ax.get_yticks()])
-# cb.ax.set_ylabel('Resistivity ($\Omega - m$)')
-# # plot a triangle
-# triangle_left = np.array([[left, -depth], [depth, -depth], [left,0], [left, -depth]])
-# triangle_right = np.array([[right, -depth], [right-depth, -depth], [right,0], [right, depth]])
-# ax3.add_patch(plt.Polygon(triangle_left,color='white'))
-# ax3.add_patch(plt.Polygon(triangle_right,color='white'))
-
-# # Subplot 5:structured constrained grid 
-# rho_contour = np.log10(np.reshape( pg.interpolate(mgr3.paraDomain, mgr3.model, grid.positions()) ,(len(mesh_y),len(mesh_x))))
-# clim = [50, 150]
-# levels = 32
-# contour_plot = ax5.contourf(mesh_X, mesh_Y, rho_contour,
-#             levels = levels,
-#             cmap='jet'
-#             ,cMin=clim[0],cMax=clim[0]
-#             )
-# ax5.set_title('Structured constrained inverted resistivity profile')
-# ax5.set_ylim(-30, 0)
-# divider = make_axes_locatable(ax5)
-# cbaxes = divider.append_axes("right", size="3%", pad=.1)
-# m = plt.cm.ScalarMappable(cmap=plt.cm.jet)
-# m.set_array(rho_contour)
-# m.set_clim(np.log10(clim[0]),np.log10(clim[1]))
-# cb = plt.colorbar(m, boundaries=np.linspace(np.log10(clim[0]),np.log10(clim[1]), levels),cax=cbaxes)
-# cb.ax.set_yticks(np.linspace(np.log10(clim[0]),np.log10(clim[1]),5))
-# cb.ax.set_yticklabels(['{:.0f}'.format(10**x) for x in cb.ax.get_yticks()])
-# cb.ax.set_ylabel('Resistivity ($\Omega - m$)')
-# # plot a triangle
-# triangle_left = np.array([[left, -depth], [depth, -depth], [left,0], [left, -depth]])
-# triangle_right = np.array([[right, -depth], [right-depth, -depth], [right,0], [right, depth]])
-# ax5.add_patch(plt.Polygon(triangle_left,color='white'))
-# ax5.add_patch(plt.Polygon(triangle_right,color='white'))
-# pg.show(interface1,ax=ax5)
-# pg.show(interface2,ax=ax5)
-# ax5.set_ylim(-30, 0)
