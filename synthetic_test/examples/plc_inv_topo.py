004a9533d09de6c5ef99870eecebdd98460fb21a
p56075607 <p56075607@gmail.com>
2024-03-12 11:27:05 +0800

    check createParaMeshPLC

--- synthetic_test/examples/plc_inv_topo.py
+++ synthetic_test/examples/plc_inv_topo.py
@@ -24,6 +24,24 @@ data['err'] = ert.estimateError(data, absoluteUError=5e-5, relativeError=0.03)
 print(data)
 ert.show(data, data['err']*100)
 
-inv = mgr.invert(data, lam=20, verbose=True, quality=33.6)
+# inv = mgr.invert(data, lam=20, verbose=True, quality=33.6)
 #mgr.showResult()
-mgr.showResult(cMin=6, cMax=10000, logScale=True, coverage=mgr.standardizedCoverage(-2))
+# mgr.showResult(cMin=6, cMax=10000, logScale=True, coverage=mgr.standardizedCoverage(-2))
+# %%
+import pygimli.meshtools as mt
+plc = mt.createParaMeshPLC(data, boundary=0.5, balanceDepth=True)
+pg.show(plc,markers=True)
+plc.positions().array()[:10]
+# %%
+plc.node(1).setPos([25, -20])
+plc.node(2).setPos([90, -5])
+pg.show(plc,markers=True)
+# %%
+plc.regionMarker(2).setPos([50, 0])
+pg.show(plc,markers=True)
+# %%
+mesh = mt.createMesh(plc, quality=34)
+data.estimateError(absoluteUError=5e-5, relativeError=0.03)
+mgr = ert.Manager(data)
+mgr.invert(mesh=mesh, verbose=True)
+mgr.showResult(cMin=20, cMax=3000, coverage=1)
