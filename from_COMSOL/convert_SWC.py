3e043283999ecb4f777460f81d402590dc79ce69
p56075607 <p56075607@gmail.com>
2024-05-18 17:07:16 +0800

    COMSOL case forward and inverse

--- from_COMSOL/convert_SWC.py
+++ from_COMSOL/convert_SWC.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 import pygimli as pg
 import pygimli.meshtools as mt
+from pygimli.physics import ert
 from scipy.interpolate import griddata
 
 csv_name = 'water content.csv'
@@ -16,16 +17,37 @@ df['resistivity'] = 1/(cFluid*df['theta']**n)
 geo = pd.read_csv('geometry.csv', header=None)
 geo.columns = ['x', 'y']
 # %%
-slope = mt.createPolygon(np.array(geo),isClosed=True, marker = 2,boundaryMarker=-1)
-mesh = mt.createMesh(slope,area=0.1)
+# slope = mt.createPolygon(np.array(geo),isClosed=True, marker = 2,boundaryMarker=-1)
+electrode_x = np.linspace(start=6, stop=24, num=25)
+electrode_y = np.linspace(start=20, stop=10, num=25)
+scheme = pg.physics.ert.createData(elecs=np.column_stack((electrode_x, electrode_y)),
+                           schemeName='dd')
+plc = mt.createParaMeshPLC(scheme,paraDepth=5,paraMaxCellSize=0.1)
+# for p in scheme.sensors():
+#     slope.createNode(p)
+#     slope.createNode(p - [0, 0.1])
+   
+mesh = mt.createMesh(plc)
 pg.show(mesh,markers=True,showMesh=True)
 
 # %%
-grid_z = griddata((df[['X', 'Y']].to_numpy()), df['resistivity'].to_numpy(), 
-                  (np.array(mesh.cellCenters())[:, :2]), method='linear')
+grid_resistivity = griddata((df[['X', 'Y']].to_numpy()), df['resistivity'].to_numpy(), 
+                  (np.array(mesh.cellCenters())[:, :2]), method='linear', fill_value=np.nan)
+fill_value = np.nanmean(grid_resistivity)
+grid_resistivity = np.nan_to_num(grid_resistivity, nan=fill_value)
+# %%
+pg.show(mesh, data=grid_resistivity, label='resistivity',cMap='jet',cMin=272,cMax=1350)
 
 # %%
-pg.show(mesh, data=grid_z, label='resistivity')
+data = ert.simulate(mesh = mesh, scheme=scheme, res=grid_resistivity, noiseLevel=1,
+                    noiseAbs=1e-6, 
+                    seed=1337)
+# %%
+ert.showData(data)
 
 # %%
+mgr = ert.ERTManager()
+mgr.invert(data=data, mesh=mesh, lam=100,verbose=True)
+
 # %%
+mgr.showResultAndFit()
